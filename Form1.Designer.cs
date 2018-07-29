c8b0df195933ea1ee0b38451fa41f88059a0343f
Vladithur <ybargatin@gmail.com>
2018-07-29 18:29:14 +0300

    Utils

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.textBox = new System.Windows.Forms.TextBox();
             this.selectFolder = new System.Windows.Forms.Button();
             this.go = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.checkBoxService = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // checkBoxExe
@@ -78,27 +80,52 @@
             this.go.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.go.Location = new System.Drawing.Point(143, 41);
             this.go.Name = "go";
-            this.go.Size = new System.Drawing.Size(279, 33);
+            this.go.Size = new System.Drawing.Size(164, 33);
             this.go.TabIndex = 5;
             this.go.Text = "CHECK";
             this.go.UseVisualStyleBackColor = true;
             this.go.Click += new System.EventHandler(this.go_Click);
             // 
+            // del
+            // 
+            this.del.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.del.Location = new System.Drawing.Point(313, 41);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(109, 33);
+            this.del.TabIndex = 6;
+            this.del.Text = "Delete md5\'s";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // checkBoxService
+            // 
+            this.checkBoxService.AutoSize = true;
+            this.checkBoxService.Location = new System.Drawing.Point(12, 82);
+            this.checkBoxService.Name = "checkBoxService";
+            this.checkBoxService.Size = new System.Drawing.Size(352, 17);
+            this.checkBoxService.TabIndex = 7;
+            this.checkBoxService.Text = "Start \"Check\" as soon as run ( Should be used if you set it as a task )";
+            this.checkBoxService.UseVisualStyleBackColor = true;
+            this.checkBoxService.CheckedChanged += new System.EventHandler(this.checkBoxService_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 86);
+            this.ClientSize = new System.Drawing.Size(434, 111);
+            this.Controls.Add(this.checkBoxService);
+            this.Controls.Add(this.del);
             this.Controls.Add(this.go);
             this.Controls.Add(this.selectFolder);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBoxExe);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(450, 125);
-            this.MinimumSize = new System.Drawing.Size(450, 125);
+            this.MaximumSize = new System.Drawing.Size(450, 150);
+            this.MinimumSize = new System.Drawing.Size(450, 150);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +138,8 @@
         private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.Button selectFolder;
         private System.Windows.Forms.Button go;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.CheckBox checkBoxService;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,7 +21,19 @@ namespace HshCheckerWindow
             try
             {
                 textBox.Text = File.ReadAllText("dir.txt");
-            } catch { }
+            }
+            catch { }
+            try
+            {
+                string[] temp = File.ReadAllText("start.txt").Split(' ');
+                if (temp[0] == "True")
+                {
+                    checkBoxExe.Checked = Convert.ToBoolean(temp[1]);
+                    checkBoxService.Checked = Convert.ToBoolean(temp[0]);
+                    check(textBox.Text, Convert.ToBoolean(temp[1]));
+                }
+            }
+            catch { }
         }
 
         private void checkBoxExe_CheckedChanged(object sender, EventArgs e)
@@ -34,28 +46,42 @@ namespace HshCheckerWindow
             check(textBox.Text, onlyExe);
         }
 
+        private void del_Click(object sender, EventArgs e)
+        {
+            List<String> files = DirSearch(textBox.Text, false, "md5hc");
+            foreach (string f in files)
+            {
+                File.SetAttributes(f, FileAttributes.Normal);
+                File.Delete(f);
+            }
+        }
+
         public static void check(string folder, bool OnlyExe)
         {
-            File.WriteAllText("dir.txt", folder);
-            List<String> files = DirSearch(folder, OnlyExe);
+            List<String> files = DirSearch(folder, OnlyExe, "");
             foreach (string f in files)
             {
                 Console.WriteLine(f);
-                if (!File.Exists(f + ".md5"))
-                    File.WriteAllText(f + ".md5", CalculateMD5(f));
+                if (!File.Exists(f + ".md5hc"))
+                {
+                    File.WriteAllText(f + ".md5hc", CalculateMD5(f));
+                    File.SetAttributes(f + ".md5hc", FileAttributes.Hidden | FileAttributes.ReadOnly);
+                }
                 else
                 {
-                    string temp0 = File.ReadAllText(f + ".md5");
+                    File.SetAttributes(f + ".md5hc", FileAttributes.Normal);
+                    string temp0 = File.ReadAllText(f + ".md5hc");
                     string temp1 = CalculateMD5(f);
                     if (!temp0.Equals(temp1))
                     {
 
-                        if (MessageBox.Show("File Chnage! Did you change the file " + f + " ?", "File Chnage", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                        if (MessageBox.Show("File Change! Did you change the file  " + f + "  ?", "File Chnage", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
                         {
-                            MessageBox.Show("Please review this file", "Danger", MessageBoxButtons.OK);
+                            MessageBox.Show("Please review this file", "Danger", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         }
                     }
-                    File.WriteAllText(f + ".md5", temp1);
+                    File.WriteAllText(f + ".md5hc", temp1);
+                    File.SetAttributes(f + ".md5hc", FileAttributes.Hidden | FileAttributes.ReadOnly);
                 }
 
             }
@@ -87,7 +113,7 @@ namespace HshCheckerWindow
             }
         }
 
-        private static List<String> DirSearch(string sDir, bool onlyExe)
+        private static List<String> DirSearch(string sDir, bool onlyExe, string fType)
         {
             List<String> files = new List<String>();
             try
@@ -96,24 +122,33 @@ namespace HshCheckerWindow
                 foreach (string f in filesInDir)
                 {
                     bool canAdd = true;
-                    if (onlyExe)
+                    if (fType == "")
                     {
-                        string subs = f.Substring(f.Length - 3);
-                        if (subs == "exe" || subs == "msi")
+                        if (onlyExe)
                         {
-                            canAdd = false;
+                            string subs = f.Substring(f.Length - 3);
+                            if (subs == "exe" || subs == "msi")
+                            {
+                                canAdd = false;
+                            }
+                        }
+                        else if (canAdd)
+                        {
+                            string subs = f.Substring(f.Length - 5);
+                            if (subs != "md5hc")
+                                files.Add(f);
                         }
                     }
-                    else if (canAdd)
+                    else
                     {
-                        string subs = f.Substring(f.Length - 3);
-                        if (subs !=  "md5")
+                        string subs = f.Substring(f.Length - fType.Length);
+                        if (subs == fType)
                             files.Add(f);
                     }
                 }
                 foreach (string d in Directory.GetDirectories(sDir))
                 {
-                    files.AddRange(DirSearch(d, onlyExe));
+                    files.AddRange(DirSearch(d, onlyExe, fType));
                 }
             }
             catch (System.Exception excpt)
@@ -122,6 +157,16 @@ namespace HshCheckerWindow
             }
 
             return files;
+        }
+
+        private void checkBoxService_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            File.WriteAllText("dir.txt", textBox.Text);
+            File.WriteAllText("start.txt", checkBoxService.Checked.ToString() + " " + onlyExe.ToString());
         }
     }
 }
